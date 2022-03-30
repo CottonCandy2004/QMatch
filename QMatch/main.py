bbf815a6918d92b0d68d8dddb6523b2d7e9d725b
CottonCandy2004 <805909642@qq.com>
2022-03-30 11:13:55 +0800

    push2

--- QMatch/main.py
+++ QMatch/main.py
@@ -3,22 +3,24 @@ import xlrd
 with open("data.txt","r") as d:
     datalistr=d.readlines()
     datalist=[]
+    result=[]
     for i in datalistr:
         datalist.append(i.strip())
-# # 这里插入excel读入
-# ex=xlrd.open_workbook(r"示例.xlsx")
-# table = ex.sheets()[1]
-# nrows = table.nrows
-# name=table.col_values(0, start_rowx=0, end_rowx=nrows)
-# satus=table.col_values(3, start_rowx=0, end_rowx=nrows)
-# e=[]
-# for i in range(nrows):
-#     if satus[i]=="--":
-#         e.append(name[i])
-# result=[]
-# for i in datalist:
-#     r=e.find(i)
-#     if r==-1:
-#         result.append(i)
-# print(result)
-print(datalist)
+        result.append(i.strip())
+# 这里插入excel读入
+ex=xlrd.open_workbook(r"示例.xlsx")
+table = ex.sheets()[1]
+nrows = table.nrows
+name=table.col_values(0, start_rowx=0, end_rowx=nrows)
+satus=table.col_values(2, start_rowx=0, end_rowx=nrows)
+e=[]
+for i in range(nrows):
+    if satus[i]=="--":
+        e.append(name[i])
+for i in datalist:
+    for a in e:
+        b=a.find(i)
+        if b!=-1:
+            result.remove(i)
+print(result)
+# print(datalist)
