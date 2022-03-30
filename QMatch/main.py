90bc954c31142582619a4e5f9af6f999d386cc4a
CottonCandy2004 <805909642@qq.com>
2022-03-30 14:26:10 +0800

    update

--- QMatch/main.py
+++ QMatch/main.py
@@ -1,27 +1,25 @@
 import xlrd
 url=input("ExcelURL:")
-with open("data.txt","r",encoding='utf-8') as d:
+with open("data.txt","r",encoding='utf-8') as d: #读入数据库
     datalistr=d.readlines()
     datalist=[]
     result=[]
     for i in datalistr:
         datalist.append(i.strip())
-        result.append(i.strip())
-# 这里插入excel读入
-ex=xlrd.open_workbook(url)
+ex=xlrd.open_workbook(url) #读入excel
 table = ex.sheets()[1]
 nrows = table.nrows
 name=table.col_values(0, start_rowx=0, end_rowx=nrows)
 satus=table.col_values(2, start_rowx=0, end_rowx=nrows)
 e=[]
-for i in range(nrows):
+for i in range(nrows): #筛出在会议室的人员
     if satus[i]=="--":
         e.append(name[i])
-for i in datalist:
+result=datalist[:]
+for i in datalist: #比对
     for a in e:
         b=a.find(i)
         if b!=-1:
             result.remove(i)
 print(result)
-# print(datalist)
 input()
